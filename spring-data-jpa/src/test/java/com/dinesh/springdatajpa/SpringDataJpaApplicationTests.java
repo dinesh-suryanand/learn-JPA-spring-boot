88ff1c26f0712b16b50fa4ff5433af669682d0f1
dinesh <dineshsuryanand.edu@gmail.com>
2022-09-27 13:51:28 +0530

    created project

---
+++ spring-data-jpa/src/test/java/com/dinesh/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
